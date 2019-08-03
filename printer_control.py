efdd9d699c94f529644bd8bc338ce46137ba64f3
Xiao Liu <xiao@albora.io>
2019-08-04 00:58:01 +0200

    make printer control as a function

--- printer_control.py
+++ printer_control.py
@@ -1,7 +1,16 @@
 from escpos.printer import Usb
 
+
 """ Seiko Epson Corp. Receipt Printer M129 Definitions (EPSON TM-T88IV) """
-p = Usb(0x04b8,0x0E15,0)
-p.text("Hello World\n")
-p.barcode('1324354657687','EAN13',64,2,'','')
-p.cut()
+
+
+def printer_print(code):
+    p = Usb(0x04b8, 0x0E15, 0)
+    text = 'Discount Code:\n\t{}\n'.format(code)
+    p.text(text)
+    p.barcode('1234567898765', 'EAN13', 64, 2, '', '')
+    p.cut()
+
+
+if __name__ == '__main__':
+    printer_print('This is working!')
