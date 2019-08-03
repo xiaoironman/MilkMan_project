9e35c3d1eb61d3aa49b077dd6833074f8ef19b0f
Xiao Liu <xiao@albora.io>
2019-08-04 00:56:56 +0200

    Add functions for gpio port control (set high and set low)

--- gpio_control.py
+++ gpio_control.py
@@ -1,12 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-def relay_control(port):
+
+def relay_control_high(port):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(port, GPIO.OUT)
     print("LED on")
     GPIO.output(port, GPIO.HIGH)
-    time.sleep(5)
+    time.sleep(1)
+
+
+def relay_control_low(port):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(port, GPIO.OUT)
     print("LED off")
     GPIO.output(port, GPIO.LOW)
+    time.sleep(1)
+
