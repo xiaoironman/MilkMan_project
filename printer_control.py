197dae5fe205df427b6e8a5c2a57d9dfdf12f8bc
Xiao Liu <xiao@albora.io>
2019-12-01 16:32:18 +0100

    change sequence of encryption and qr_gen

--- printer_control.py
+++ printer_control.py
@@ -133,18 +133,20 @@ def gen_qr(input: str, num_code: int):
     return qr_name
 
 
-def json_format(text: str) -> str:
-    return '{{\n\t\"recycle_codes:\": [\n\t\"{}\"\n\t]\n}}'.format(text)
+def json_format(text_list: list) -> str:
+    text = ''
+    for i in text_list:
+        text += i + ';'
+    return '{{\n\t\"recycle_codes\": [\n\t\"{}\"\n\t]\n}}'.format(text)
 
 
 def gen_qr_main(key: str, num_of_code: int, code_length: int = 15):
-    res = ''
-    for i in range(num_of_code):
-        res += gen_code(code_length) + ';'
+    res = []
     key = key.encode('utf-8')
     eg = EncryptData(key)
-    res_encrypted = eg.encrypt(res)
-    qr_name = gen_qr(json_format(res_encrypted), num_of_code)
+    for i in range(num_of_code):
+        res.append(eg.encrypt(gen_code(code_length)))
+    qr_name = gen_qr(json_format(res), num_of_code)
     return qr_name
 
 
